ad3836fa6a84fa11cbb547f116238a76b85ec609
apostolides <thanosapostolidis@protonmail.com>
2020-12-26 20:02:55 +0200

    Sample riot api calls.

---
+++ src/main/java/gr/hua/distsys/DistSysSec/Utilities/Requests.java
@@ -0,0 +1,44 @@
+package gr.hua.distsys.DistSysSec.Utilities;
+
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public abstract class Requests {
+
+    public static String get(String url) {
+
+        HttpURLConnection urlConnection;
+        URL targetUrl;
+        int responseCode;
+
+        String responseContent = "";
+
+        try {
+            targetUrl = new URL(url);
+            urlConnection = (HttpURLConnection) targetUrl.openConnection();
+            urlConnection.setRequestMethod("GET");
+
+            urlConnection.setConnectTimeout(30000);
+            urlConnection.setReadTimeout(30000);
+
+            responseCode = urlConnection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) { // Success
+
+                Scanner scan = new Scanner(new InputStreamReader(urlConnection.getInputStream()));
+
+                while (scan.hasNextLine()) {
+                    responseContent += scan.nextLine();
+                }
+
+                scan.close();
+            } else {
+                return null;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return responseContent;
+    }
+}
