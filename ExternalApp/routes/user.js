a1cafd22a1aecb448353de73d86d275102964308
apostolides <thanosapostolidis@protonmail.com>
2020-12-30 20:40:46 +0200

    External-Internal Sample Call

--- ExternalApp/routes/user.js
+++ ExternalApp/routes/user.js
@@ -2,8 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const jwtSecurity = require('../security/jwt.js');
+const { default: Axios } = require('axios');
 
 const router = express.Router();
+const internalSystemEndpoint = "http://localhost:8080/user"; 
 
 router.use(bodyParser.json());
 router.use((error,_req,res,next)=>{
@@ -18,8 +20,20 @@ router.use((error,_req,res,next)=>{
 router.use(cookieParser());
 
 router.route('/')
-    .get(jwtSecurity.authenticateToken,(_req,res)=>{
-    res.render('home',{username:res.locals.payload.sub});
+    .get(jwtSecurity.authenticateToken,(req,res)=>{
+        let responseJson;
+        let headers = {'Authorization':`Bearer ${req.cookies.LOHTOKEN}`};
+        Axios.get(internalSystemEndpoint,{headers:headers})
+        .then(response =>{
+            responseJson = response.data;
+        })
+        .catch(error=>{
+            console.error(error);
+            responseJson = null;
+        })
+        .finally(()=>{
+            res.render('home',{username:res.locals.payload.sub,internalSystemResponse:responseJson});
+        }); 
 });
 
 module.exports = router;
