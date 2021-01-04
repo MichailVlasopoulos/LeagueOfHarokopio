8a29d8fcc494b5cfa97a30d6ce1b1e4179768d8e
apostolides <thanosapostolidis@protonmail.com>
2021-01-04 22:50:16 +0200

    Successful login redirections.

--- ExternalApp/public/js/login.js
+++ ExternalApp/public/js/login.js
@@ -11,7 +11,7 @@ document.getElementById("loginButton").addEventListener('click',()=>{
             password:password
         })
         .then(response=>{
-            document.location = '/user';
+            document.location = '/home';
         })
         .catch(error=>{
             resultField.innerHTML = '';
