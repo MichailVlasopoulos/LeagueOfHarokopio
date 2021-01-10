6111407836f0219819f27f3f674bb37251568b6f
MichailVlasopoulos <MihalisVls@hotmail.com>
2021-01-10 23:54:01 +0200

    Service : Premium User Service fixes

--- src/main/java/gr/hua/DistSysApp/ritoAPI/Repositories/AuthoritiesRepository.java
+++ src/main/java/gr/hua/DistSysApp/ritoAPI/Repositories/AuthoritiesRepository.java
@@ -14,16 +14,16 @@ public interface AuthoritiesRepository extends JpaRepository<Authorities,Integer
     @Query(value = "SELECT * FROM Authorities WHERE role=ROLE_USER OR role=ROLE_PREMIUM_USER ",nativeQuery = true)
     List<Authorities> getAllUsers();
 
-    @Query(value = "SELECT * FROM Authorities WHERE role=ROLE_USER_ADMIN OR role=ROLE_PremiumAdmin OR role=ROLE_MAIN_ADMIN ",nativeQuery = true)
+    @Query(value = "SELECT * FROM Authorities WHERE role=ROLE_USER_ADMIN OR role=ROLE_PREMIUM_ADMIN OR role=ROLE_MAIN_ADMIN ",nativeQuery = true)
     List<Authorities> getAllAdmins();
 
     @Modifying(clearAutomatically = true)
     @Transactional
     @Query(value = "UPDATE Authorities SET role=ROLE_PREMIUM_USER WHERE user_id=?", nativeQuery = true)
-    Integer goPremium(int user_id);
+    void goPremium(int user_id);
 
     @Modifying(clearAutomatically = true)
     @Transactional
     @Query(value = "UPDATE Authorities SET role=ROLE_USER WHERE user_id=?", nativeQuery = true)
-    Integer cancelPremium(int user_id);
+    void cancelPremium(int user_id);
 }
